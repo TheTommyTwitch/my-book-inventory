b9209f1d80e55e300d64c0eb31d2eacd280fb3aa
Thomas Asadurian <Tasadurian@gmail.com>
2015-08-14 21:37:20 -0700

    seperated controllers

---
+++ app/controllers/api-books.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Book = mongoose.model('Book');
+
+module.exports = function(app) {
+  app.use('/api', router);
+};
+
+router.use(function(req, res, next) {
+  console.log('Something is happening');
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json({
+    message: 'Api Page!'
+  });
+});
+
+router.route('/books')
+  .post(function(req, res) {
+    var book = new Book();
+    book.title = req.body.title;
+    book.isbn = req.body.isbn;
+    book.author = req.body.author;
+
+    book.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({
+        message: 'Book created!'
+      });
+    });
+  });
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -17,19 +17,3 @@ router.get('/', function(req, res) {
     message: 'Home Page!'
   });
 });
-
-router.route('/api/books')
-  .post(function(req, res) {
-    var book = new Book();
-    book.title = req.body.title;
-    book.isbn = req.body.isbn;
-    book.author = req.body.author;
-
-    book.save(function(err) {
-      if (err)
-        res.send(err);
-      res.json({
-        message: 'Book created!'
-      });
-    });
-  });
