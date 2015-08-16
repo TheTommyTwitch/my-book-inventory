ebe01cf7be683741f2c500e0f340283ec11e4423
Thomas Asadurian <Tasadurian@gmail.com>
2015-08-16 13:49:25 -0700

    angular error fixed

---
+++ public/js/services/app.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "../views/home.html"
+    });
+});
--- public/js/services/routes.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app', ['ngRoute']);
-
-app.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: '../views/home.html',
-      controller: 'mainController'
-    });
-});
