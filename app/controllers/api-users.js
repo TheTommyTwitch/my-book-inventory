48ecb5ac83bd8b2f4665cc6c14f6a255d996aa4a
Thomas Asadurian <Tasadurian@gmail.com>
2015-08-17 19:53:17 -0700

    added users api

---
+++ app/controllers/api-users.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
+
+module.exports = function(app) {
+  app.use('/api', router);
+};
+
+router.use(function(req, res, next) {
+  console.log('Something is happening');
+  next();
+});
+
+router.route('/users')
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if (err)
+        res.send(err);
+      res.json(users);
+    });
+  });
+
+router.route('/users/:user_id')
+  .get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err)
+        res.send(err);
+      res.json(user);
+    });
+  });
